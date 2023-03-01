41e6f6fe19d18d641a2a6b8b44ed1f1d7493ef3c
Oleh <koroboffpl@gmail.com>
2023-03-01 17:54:05 +0200

    films.sort

--- project/js/script.js
+++ project/js/script.js
@@ -1,9 +1,12 @@
 
 'use strict';
 // 1) Удалить все рекламные блоки со страницы (правая часть сайта)
-const promoBlocks = document.querySelector("div.promo__adv > img"),
+const promoBlocks = document.querySelectorAll('.promo__adv img'),
       promoBG = document.querySelector(".promo__bg");
-      promoBlocks.remove();
+
+promoBlocks.forEach(item => {
+    item.remove();
+});
 // 2) Изменить жанр фильма, поменять "комедия" на "драма" 
 document.querySelector(".promo__genre").innerHTML = "драма";
 // 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
@@ -22,9 +25,14 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-let promInter = document.querySelectorAll(".promo__interactive-item");
-movieDB.forEach(function(item, i, movieDB){ 
-    promInter.insertAdjacentHTML("afterend", "<li>"+item+[i + 1]+"</li>");
+const promInter = document.querySelector('.promo__interactive-list');
+//const newLi = document.getElementsByClassName('promo__interactive-list');
+const sortMovie = movieDB.movies.sort();
+promInter.innerHTML = "";
+sortMovie.forEach((film, i) => {
+    promInter.innerHTML += `
+        <li>${i + 1} ${film}</li>
+    `;
 });
 
 
