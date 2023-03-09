ed5e3e643152dcde9c23be476dbfbd5b368e711e
Oleh <koroboffpl@gmail.com>
2023-03-09 18:11:31 +0200

    push in array

--- project/js/script.js
+++ project/js/script.js
@@ -1,20 +1,19 @@
 
 'use strict';
-// 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+document.addEventListener('DOMContentLoaded', () => {
+    
 const promoBlocks = document.querySelectorAll('.promo__adv img'),
       promoBG = document.querySelector(".promo__bg");
 
 promoBlocks.forEach(item => {
     item.remove();
 });
-// 2) Изменить жанр фильма, поменять "комедия" на "драма" 
+
 document.querySelector(".promo__genre").innerHTML = "драма";
-// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-//Реализовать только при помощи JS
+
 promoBG.style.cssText = 'background: url(img/bg.jpg) center center/cover no-repeat;'
-// 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-//  Отсортировать их по алфавиту 
-// 5) Добавить нумерацию выведенных фильмов
+
 
 const movieDB = {
     movies: [
@@ -28,13 +27,47 @@ const movieDB = {
 const promInter = document.querySelector('.promo__interactive-list');
 //const newLi = document.getElementsByClassName('promo__interactive-list');
 const sortMovie = movieDB.movies.sort();
-promInter.innerHTML = "";
-sortMovie.forEach((film, i) => {
-    promInter.innerHTML += `
-    <li class="promo__interactive-item">${i + 1} ${film}
-        <div class="delete"></div>
-    </li>
-    `;
+
+
+let add = document.getElementById("btnflm");
+
+
+add.addEventListener("click", function(event) {
+    event.preventDefault();
+    let input = document.querySelector('.adding__input').value;
+   // movieDB.movies.push(input);
+   
+    if (input) {
+
+        if (input.length > 21) {
+            input = `${input.substring(0, 22)}...`;
+        }
+
+       
+        movieDB.movies.push(input);
+     
+
+       
+    }
+
+    createMovieList(movieDB.movies, promInter);
+    });
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+        movieDB.movies.sort();
+    }
+
+  
+
 });
 
 
